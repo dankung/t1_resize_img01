96eb4b474c0d32c9b8832a9d4794dd1ce560a927
Daniel Sutoro <17750152+dankung@users.noreply.github.com>
2022-05-16 18:47:30 +0700

    Add files via upload
    Add unit test

--- t1_resize_img01/test/test_main.cc
+++ t1_resize_img01/test/test_main.cc
@@ -1,10 +1,73 @@
 #define DROGON_TEST_MAIN
 #include <drogon/drogon_test.h>
 #include <drogon/drogon.h>
+#include <fstream>
+#include <memory>
+#include "../controllers/image_converter.h"
+#include "../controllers/TaskController.h"
 
 DROGON_TEST(BasicTest)
 {
     // Add your tests here
+}
+
+DROGON_TEST(RemoteAPITest)
+{
+   auto client = HttpClient::newHttpClient("http://localhost:8080");
+   //auto req = HttpRequest::newHttpRequest();
+   //auto req = HttpRequest::
+   std::string all_headers;
+   std::string imgbase64;
+   int desired_width;
+   int desired_height;
+
+   //req->setPath("/resize_image/");
+   std::ifstream is ("encode.bin", std::ifstream::binary);
+   if (is) {
+        is.seekg (0, is.end);
+        int length = is.tellg();
+        is.seekg (0, is.beg);
+
+        std::unique_ptr<char[]> buffer{new char [length]}; // note: use char[] as parameter
+        is.read (buffer.get(), length);
+        // cout << buffer.get() << endl;
+        imgbase64 = buffer.get();
+        desired_width = 300;
+        desired_height = 200;
+
+        Json::Value ret;        
+         
+        //ret["input_jpeg"]= imgbase64;
+	ret["desired_height"]=desired_height;		
+        ret["desired_width"]= desired_width;
+        ret["input_jpeg"]=imgbase64;
+        //ret["desired_height"]= desired_height;
+        
+       //req.newHttpJsonRequest(ret); 
+       //auto req1=HttpRequest::newHttpJsonRequest(ret);
+       auto req  =  HttpRequest::newCustomHttpRequest(ret);
+       req->setPath("/resize_image/");  
+       req->setMethod(Post);
+ 
+       //req->jsonObject(req1);   
+       //all_headers = req->headers();
+       // LOG_DEBUG << " isi json= " << req->getBody() << " end";             
+       // LOG_DEBUG << " isi client host= " << client->getHost() << " end";     
+
+       // CHECK(req->path() == "/resize_image/");
+       // CHECK(req->contentType() == CT_APPLICATION_JSON);
+       client->sendRequest(req, [TEST_CTX](ReqResult res, const HttpResponsePtr& resp) {
+       		// LOG_DEBUG << " ReqResult= " << ReqResult::Ok << " end";
+                // LOG_DEBUG << " resp != nullptr " <<  (resp != nullptr) << " end";
+
+                REQUIRE(res == ReqResult::Ok);
+       		REQUIRE(resp != nullptr);
+		
+       		CHECK(resp->getStatusCode() == k200OK);
+       		CHECK(resp->contentType() == CT_APPLICATION_JSON);
+   	});
+   }
+     
 }
 
 int main(int argc, char** argv) 
